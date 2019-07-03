60cb1f1ade0163bd0efd114f61d982ee9e6e3638
yacine <yacineyacine31212@gmail.com>
2019-07-03 13:33:53 +0000

    display user function

--- public/index.php
+++ public/index.php
@@ -1,3 +1,41 @@
-<?php
-require_once("../ressources/config.php");
-?>
+<?php  require_once("../ressources/config.php"); ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ide-Parc</title>
+</head>
+<body>
+    <div class="add-emp">
+        <h3>ajouter un employé</h3>
+        <form action="/" methode="POST">
+        <input type="text" name="fName" placeholder="nom" required eutofocus>
+        <br>
+        <br>
+        <input type="text" name="lName" placeholder="prénom" required>
+        <br>
+        <br>
+        <input type="date" name="dob" required>
+        <br>
+        <br>
+        <input type="text" name="addres" placeholder="address" required>
+        <br>
+        <br>
+        <input type="email" name="email" placeholder="email" required>
+        <br>
+        <br>
+        <input type="text" name="category" placeholder="category" required>
+        <br>
+        <br>
+        <button type="submit">ajouter</button>
+        </form>
+    </div>
+    <!--Display employies -->
+    <?php
+        get_employies();
+    ?>
+    
+</body>
+</html>
--- ressources/functions.php
+++ ressources/functions.php
@@ -1,3 +1,49 @@
 <?php
- echo "echo from functions.php";
+          //helper functions  
+   
+ // return the result for a given quey
+ function query($sql){
+     global $connection;
+     return mysqli_query($connection, $sql);
+ };
+ // verify if the query is well executed for a given result
+ function confirm($result){
+     if (!$result) {
+         global $connection;
+         die("FAILED CONNECTION" . mysqli_error($conection));
+     }
+ };
+ // escape some carracters to prevent SQL injection
+ function escape_string($string){
+     global $connection;
+     return mysqli_real_escape_string($connection, $string);
+ };
+ // fetch data from result of query
+ function fetch_array($result){
+      return mysqli_fetch_array($result);
+ };
+
+ // Get product
+ function get_employies(){
+     $query = query("SELECT * FROM employe");
+     confirm($query);
+     while ($row = fetch_array($query)) {
+         echo "<div> 
+                     <h4>nom: </h4>          
+                     <p>" . $row['nom'] ."</p>
+                     <h4>prenom: </h4>           
+                     <p>" . $row['prenom'] ."</p>
+                     <h4>date de naissance: </h4>
+                     <p>" . $row['ddn']."</p>
+                     <h4>address: </h4>
+                     <p>".   $row['adress'] ."</p>
+                     <h4>telephone: </h4> 
+                     <p>".  $row['tel'] . "</p>
+                     <h4>email: </h4>
+                     <p>" . $row['email']."</p>
+                     <img src=".$row['photo'] . " alt=" . $row['nom']. " " . $row['prenom']."</div>";
+               //echo $employe;    
+ }
+}
+
 ?>
